86bc4afd2f9bb15bb81757b584ee34c449691272
0802222 <0802222@naver.com>
2025-02-14 03:53:41 +0900

    Git과 Github

---
+++ src/test/kotlin/com/dongdong/portfolio/PortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dongdong.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
